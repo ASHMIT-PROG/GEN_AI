c652c5ba2f9db50edf01ca1037a87a6ce1d90be9
ASHMIT-PROG <thatcodingguy1616@gmail.com>
2026-05-28 10:44:26 +0530

    fixed login issue

--- PYTHON/PROJECTS/login_access_(if_else)_6.py
+++ PYTHON/PROJECTS/login_access_(if_else)_6.py
@@ -3,16 +3,15 @@ password=input("Enter password: ")
 
 if(password.isdigit()):#check that the password entered is digit or not , works only on string
 
-    if(username == "raman"):
-
-        if(int(password) == 123):
-            print("Login Successful")
-
-        else:
-            print("Wrong Password")
+    if(username == "raman" and int(password)==123):
+        print("login successful")
 
     else:
-        print("User Not Found")
+        print("Wrong Password")    
+
 
 else:
-    print("INT values only")
+        print("User Not Found")
+
+
+
